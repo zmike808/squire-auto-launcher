01a63b86ce358eb5bcf0bddbc1f45659f346a838
Michael Zemsky <zmike808@gmail.com>
2023-03-23 12:09:48 -0400

    updates

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # squire-auto-clients
- auto run multiple squire clients ezclap
+auto run multiple squire clients ezclap
+
+# How to use
+exe of the .py included for those who can't python.
+put inside your squire-launcher folder and edit th accounts.txt file to your liking.
+if you don't understand what to put in to the accounts.txt file, you shouldn't be using this.
+
+enjoy.
--- run_clients.py
+++ run_clients.py
@@ -2,17 +2,17 @@ import subprocess
 
 from pathlib import Path
 
-import pyautogui
-import pywinauto
+# import pyautogui
+# import pywinauto
 import time
 
-from ahk import AHK
+# from ahk import AHK
 
-ahk = AHK()
+# ahk = AHK()
 ## CHANGE THESE TWO VARIABLES TO YOUR OWN JAVA AND SQUIRE.JAR LOCATION!!!!!!!!!!!!
-JAVA_PATH = "java"
+JAVA_PATH = Path("./jre/bin/java")
 
-SQUIRE_JAR_PATH = "squire.jar"
+SQUIRE_JAR_PATH = Path("./jar/squire-launcher.jar")
 ## CHANGE THESE TWO VARIABLES TO YOUR OWN JAVA AND SQUIRE.JAR LOCATION!!!!!!!!!!!!
 procs: list[subprocess.Popen] = []
 def run_clients():
@@ -24,23 +24,36 @@ def run_clients():
         parser = argparse.ArgumentParser()
         parser.add_argument("--world", type=int, default=0, help="World to log all accounts into")
         parser.add_argument("--proxy", type=str, default=None, help="Proxy to use for all accounts")
-        parser.add_argument("--skip", type=bool, default=False, help="automatically skip auth and nightly")
+        parser.add_argument("--skip", type=bool, default=True, help="automatically skip auth and nightly")
         args = parser.parse_args()
         main_arg = f"{JAVA_PATH} -XX:+DisableAttachMechanism -Dsun.java2d.noddraw=true -Xmx4G -Xss2m -XX:CompileThreshold=1500 -jar -Drunelite.launcher.nojvm=true {SQUIRE_JAR_PATH}"
+        print(f"base launching arguements {main_arg}")
         if args.skip:
             main_arg = f"{main_arg} --skip-auth --nightly"
         for client in clients:
             if client.startswith("#"):
                 continue
             launch_arg = main_arg
+            print(f"to split {client} predicted result {client.split(',')}")
             accinfo = client.split(",")
             print(accinfo)
-            username = accinfo[0].strip()
-            password = accinfo[1].strip()
+            if len(accinfo) == 0 or len(accinfo) == 1:
+                run_arg = f"{launch_arg}"
+                if len(accinfo) == 1:
+                    coloncount = len([x for x in accinfo[0] if x == ":"])
+                    if coloncount >= 1:
+                        run_arg = f"{launch_arg} --proxy={accinfo[0]}"
+                print(f"proxy only : {run_arg}")
+                proc: subprocess.Popen = subprocess.Popen(run_arg)#, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, start_new_session=True)
+                procs.append(proc)
+                time.sleep(1)
+                continue
+            username = accinfo[0]
+            password = accinfo[1]
             if len(accinfo) > 2:
-                world = accinfo[2].strip()
+                world = accinfo[2]
             if len(accinfo) > 3:
-                proxy = accinfo[3].strip()
+                proxy = accinfo[3]
             if args.world != 0:
                 print(f"adding parameter world arg {args.world}")
                 launch_arg = f"{launch_arg} --world={args.world}"
@@ -53,20 +66,20 @@ def run_clients():
             elif proxy:
                 print(f"adding account world arg {proxy}")
                 launch_arg = f"{launch_arg} --proxy={proxy}"
-            userproperties =Path().home().joinpath(f".squire/{username}.squire.properties")
-            print(userproperties.absolute())
-            squireprops = Path().home().joinpath(".squire/squire.properties")
-            print(squireprops.absolute())
-            if not userproperties.exists():
-                import shutil
-                shutil.copy(squireprops.absolute(), userproperties.absolute())
-            run_arg = f"{launch_arg} --account={username}:{password}"# --profile={userproperties.absolute()}"
+            # userproperties =Path().home().joinpath(f".squire/{username}.squire.properties")
+            # print(userproperties.absolute())
+            # squireprops = Path().home().joinpath(".squire/squire.properties")
+            # print(squireprops.absolute())
+            # if not userproperties.exists():
+            #     import shutil
+            #     shutil.copy(squireprops.absolute(), userproperties.absolute())
+            run_arg = f"{launch_arg} --account=\"{username}:{password}\""# --profile={userproperties.absolute()}"
             print(run_arg)
-            print(run_arg.split(" "))
-
-            proc: subprocess.Popen = subprocess.Popen(run_arg.split(" "), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, start_new_session=True)
+        # print(run_arg.split(" "))
+            
+            proc: subprocess.Popen = subprocess.Popen(run_arg)#, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, start_new_session=True)
             procs.append(proc)
-        # time.sleep(1)
+            time.sleep(1)
 # def window_filter(window):
 #     return "java" in window.get('process')
 #
